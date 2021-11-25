5012c20563f8c235a9189822b2aa1e54f36b3b53
Naufal <naufal@ubuntu>
2021-11-25 10:43:38 +0530

    updates db

--- core_schema.sql
+++ core_schema.sql
@@ -1,18 +1,58 @@
-CREATE TABLE IF NOT EXISTS user_cred (
+CREATE TABLE IF NOT EXISTS cred (
     user_id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(254) NOT NULL,
     password VARCHAR(18) NOT NULL,
     type VARCHAR(10) NOT NULL
 );
 
-DROP TABLE posts;
+CREATE TABLE IF NOT EXISTS client_cred (
+    id SERIAL PRIMARY KEY REFERENCES cred(user_id) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age VARCHAR(3),
+    city VARCHAR(50),
+    occupation VARCHAR(20),
+    concerns VARCHAR,
+    phonenumber VARCHAR(20),
+    emergency_contact VARCHAR(20),
+    relationship_status VARCHAR(50),
+    status VARCHAR(15),
+    therapist VARCHAR,
+    timeperiod VARCHAR(50),
+    gender VARCHAR(50)
+);
+
+
+CREATE TABLE IF NOT EXISTS therapist_cred (
+    id SERIAL PRIMARY KEY REFERENCES cred(user_id) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    languages VARCHAR NOT NULL,
+    specializations VARCHAR NOT NULL,
+    mode VARCHAR NOT NULL,
+    medium VARCHAR NOT NULL,
+    phonenumber VARCHAR NOT NULL
+);
+
+
 
 CREATE TABLE IF NOT EXISTS posts (
     post_id SERIAL PRIMARY KEY NOT NULL,
+    user_id SERIAL REFERENCES cred(user_id) NOT NULL,
     title VARCHAR(100) NOT NULL,
-    content VARCHAR(2000) NOT NULL,
-    user_id SERIAL REFERENCES user_cred(user_id),
-    tags VARCHAR(500),
-    time_stamp VARCHAR(50)
+    description VARCHAR(2000) NOT NULL,
+    tags VARCHAR(500) NOT NULL,
+    date VARCHAR(50) NOT NULL,
+    time VARCHAR(50) NOT NULL
+    
 );
 
+
+CREATE TABLE IF NOT EXISTS matches (
+    id SERIAL PRIMARY KEY NOT NULL,
+    client_id SERIAL REFERENCES cred(user_id) NOT NULL,
+    therapist_id SERIAL REFERENCES cred(user_id) NOT NULL,
+    start_date VARCHAR(50) NOT NULL,
+    end_date VARCHAR(50),
+    status VARCHAR(20) NOT NULL
+);
