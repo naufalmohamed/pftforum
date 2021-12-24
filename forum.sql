95e6094fcc92c7d899595eda08b93e9b96aee245
Naufal <naufal@ubuntu>
2021-12-24 17:36:44 +0530

    adds forum.sql

---
+++ forum.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS events (
+    event_id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT(2000) NOT NULL,
+    date VARCHAR(50),
+    time VARCHAR(50),
+    url_local VARCHAR,
+    event_link VARCHAR,
+    timestamp VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS internships (
+    intern_id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT(2000) NOT NULL,
+    date VARCHAR(50),
+    time VARCHAR(50),
+    url_local VARCHAR,
+    intern_link VARCHAR,
+    timestamp VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS survey (
+    survey_id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT(2000) NOT NULL,
+    date VARCHAR(50),
+    time VARCHAR(50),
+    url_local VARCHAR,
+    survey_link VARCHAR,
+    timestamp VARCHAR(50)
+);
